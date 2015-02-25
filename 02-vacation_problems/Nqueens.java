bfc9e2f179a189df4cc536696639bba67dd166f1
kmejia <kmejia345@gmail.com>
2015-02-24 23:39:30 -0500

    tour

---
+++ 02-vacation_problems/Nqueens.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Nqueens{
+
+    private char[][] board;
+    private boolean = false;
+    public void delay(){
+	try {
+	    Thread.sleep(100);
+	} catch (Exception e) {}
+    }
+    public void solve(int n){
+	//	if (n==2 || n==3) {System.out.println("There are no varying solutions");}
+	System.out.print(this);
+    }
+    
+}
