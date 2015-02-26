9de6ca7ce350e6c48f311698d6a9fb4e89f1b3fc
kmejia <kmejia345@gmail.com>
2015-02-26 02:24:48 -0500

    not functional

--- 02-vacation_problems/Knights.java
+++ 02-vacation_problems/Knights.java
@@ -3,14 +3,9 @@ import java.io.*;
 public class Knights{
     
     private char[][] board = new char[5][5];
-    // public Knights() {
-    // 	//	width = 5;
-    // }   
-    // public Knights(int w) {
-    // 	width = w;
-    // }
+   
     private boolean done = false;
-    private int moves;
+    private int moves=0;
     public void fill() {
 	for (int i =0;i<board.length;i++) {
 	    for (int j =0;j<board.length;j++){
@@ -38,11 +33,41 @@ public class Knights{
 	return ans;
     }
 
- 
+
+    public void solve(int x, int y){
+	if (board[x][y]<0 ||
+	    board[x][y]=='a' ||
+	    board[x][y]<moves|
+	    done){
+	    return;
+	}
+	
+	if (board[x][y]=='0'||moves>25){
+	    System.out.println(this);
+	    done = true;
+	}
+	delay();
+	System.out.println(this);
+	board[x][y]='k';
+	solve(x+1,y-2);
+	solve(x-1,y+2);
+	solve(x+1,y+2);
+	solve(x+2,y-1);	
+	solve(x+2,y+1);
+
+	solve(x-2,y+1);
+	solve(x-2,y-1);
+	moves++;
+	if (!done){
+	    board[x][y]=moves;
+	    moves--;
+	}
+    }
     public static void main(String[] args) {
 	Knights k = new Knights();
 	k.fill();
 	System.out.println(k);
+	k.solve(0,0);
     }
  
 }
