83ffd30fecaabac220291427a13476a3493c7823
kmejia <kmejia345@gmail.com>
2015-03-19 08:42:13 -0400

    l

---
+++ 08-dummy/LLit.java
@@ -0,0 +1,13 @@
+public class LLit() {
+	private Node l;
+	public LLit(Node n) {
+	l = n.getNext(); 
+	}
+	public boolean hasNext(){
+		return (l!=null);
+	}
+/* move to next Node and  return the value in the node (before the move) */
+	public int next(){
+	
+	}
+}
