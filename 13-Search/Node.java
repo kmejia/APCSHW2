752dcebd5ebee87d71a066724d01bd1574a9f09e
kmejia <kmejia345@gmail.com>
2015-04-19 22:51:55 -0400

    astar

--- 13-Search/Node.java
+++ 13-Search/Node.java
@@ -1,11 +1,16 @@
-
+import java.io.*;
+import java.util.*;
 public class Node {
     private int x,y;
     private Node prev;
+    public double d;
     private int p;
-    public Node(int x, int y){
+    private int steps;
+    public Node(int x, int y, int s){
 	this.x = x;
 	this.y = y;
+        setM();
+	this.steps=s;
     }
     public Node getPrev() {
 	return prev;
@@ -13,11 +18,28 @@ public class Node {
     public void setPrev(Node n){
 	prev = n;
     }
+    public void setM(){
+	p = (26 -x) + (18- y); 
+    }
+    public void setD(){
+	d= Math.sqrt((26-x) *(26-x) + (18-y) * (18-y));
+    }
     public int getX() {
 	return x;
     }
     public int getY() {
 	return y;
+    }
+    public int getP(){
+	return p;
+    }
+    
+    public int getPriority(){
+	return p + steps;
+    }
+    public double getD(){return d;}
+    public int getS(){
+	return steps;
     }
 }
 
